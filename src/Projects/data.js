57b36efe88c78af449535fa72f80844476bad225
Xavi-Alejando <79874223+Xavi-Alejando@users.noreply.github.com>
2023-09-06 21:33:11 -0400

    Removed non-working projects and re-included real estate site

--- src/Projects/data.js
+++ src/Projects/data.js
@@ -2,7 +2,8 @@ module.exports.data = [
   {
     id: 1,
     projectTitle: `Xavi's music lounge`,
-    gifSource: "https://portfoliogifs.s3.us-east-2.amazonaws.com/XavisMusicLounge.gif",
+    gifSource:
+      "https://portfoliogifs.s3.us-east-2.amazonaws.com/XavisMusicLounge.gif",
     link: `https://legendary-tartufo-7b1cd1.netlify.app/register`,
     projectDescription: [
       {
@@ -21,38 +22,56 @@ module.exports.data = [
   },
   {
     id: 2,
-    projectTitle: `Room of Chat`,
-    gifSource: "https://portfoliogifs.s3.us-east-2.amazonaws.com/RoomOfChat.gif",
-    link: `https://spontaneous-manatee-291ab8.netlify.app/`,
+    projectTitle: `Real estate portfolio template`,
+    gifSource:
+      "https://portfoliogifs.s3.us-east-2.amazonaws.com/portfolioTemplate.png",
+    link: `http://portfolio-template.s3-website.us-east-2.amazonaws.com/`,
     projectDescription: [
       {
         id: 1,
-        data: `Spark a conversation with someone from across the world. Whether it be anonymously or as yourself.`,
+        data: `A portfolio template aimed at real estate agents to promote themselves to their clients.`,
       },
 
       {
         id: 2,
-        data: `Created with Angular and Node, this app integrates Socket.io to cross communicate between clients.`,
+        data: `Created with JQuery and HTML/CSS, and hosted on Amazon Web Services (AWS) for reliability.`,
       },
     ],
   },
-  {
-    id: 3,
-    projectTitle: `Trip Visualizer`,
-    gifSource: "https://portfoliogifs.s3.us-east-2.amazonaws.com/TripVisualizer.gif",
-    link: `https://gleaming-paprenjak-ac7819.netlify.app/#`,
-    projectDescription: [
-      {
-        id: 1,
-        data: `A simple trip visualizer with the ability to retrieve trip information and display it in a beautiful way.`,
-      },
+  // {
+  //   id: 2,
+  //   projectTitle: `Room of Chat`,
+  //   gifSource: "https://portfoliogifs.s3.us-east-2.amazonaws.com/RoomOfChat.gif",
+  //   link: `https://spontaneous-manatee-291ab8.netlify.app/`,
+  //   projectDescription: [
+  //     {
+  //       id: 1,
+  //       data: `Spark a conversation with someone from across the world. Whether it be anonymously or as yourself.`,
+  //     },
 
-      {
-        id: 2,
-        data: `Created with JQuery and HTML/CSS.`,
-      },
-    ],
-  },
+  //     {
+  //       id: 2,
+  //       data: `Created with Angular and Node, this app integrates Socket.io to cross communicate between clients.`,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 3,
+  //   projectTitle: `Trip Visualizer`,
+  //   gifSource: "https://portfoliogifs.s3.us-east-2.amazonaws.com/TripVisualizer.gif",
+  //   link: `https://gleaming-paprenjak-ac7819.netlify.app/#`,
+  //   projectDescription: [
+  //     {
+  //       id: 1,
+  //       data: `A simple trip visualizer with the ability to retrieve trip information and display it in a beautiful way.`,
+  //     },
+
+  //     {
+  //       id: 2,
+  //       data: `Created with JQuery and HTML/CSS.`,
+  //     },
+  //   ],
+  // },
   // {
   //   id: 4,
   //   projectTitle: `Real estate portfolio template`,
